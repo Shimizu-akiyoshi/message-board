a9af04ede84e1a214cebc28f021728d87843bd79
shimizu_akiyoshi <acho.0314@gmail.com>
2015-10-03 03:02:38 +0000

    Implementation of the validation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,25 @@
 class MessagesController < ApplicationController
   def index
     @messages = Message.all
+    @message = Message.new
   end
 
 
   def create
-    @message = Message.new(message_params)
-    @message.save
-    redirect_to root_path , notice: "Saved message"
+      @message = Message.new(message_params)
+    if @message.save
+      redirect_to root_path , notice: "Saved message"
+    else
+      @messages = Message.all
+      flash.now[:alert] = "Sorry, it failed to save the message"
+      render 'index'
+    end
   end
-  
+ 
   private 
   def message_params
     params.require(:message).permit(:name, :body)
   end
-
 end
+
 
